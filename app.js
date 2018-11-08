11dad909ee4ea96036c8d7ab4cfa6bd6a4b42967
harmeet651 <harmeet651@gmail.com>
2018-11-08 12:34:01 -0800

    documentation and indentation added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express'); //Express is the backend part of the MEAN stack, together with MongoDB database and AngularJS frontend framework. This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+var bodyParser = require('body-parser'); //body-parser extract the entire body portion of an incoming request stream and exposes it on req.body . This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
 var path = require('path');
 var request = require('request');
 //init app
@@ -7,90 +7,86 @@ var app = express();
 
 //body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //set static path
 app.use(express.static(path.join(__dirname, 'public')))
 
 //home route
-app.get('/', function(req, res){
-  res.render('public');
+app.get('/', function (req, res) {
+	res.render('public');
 });
 
-app.get("/allData", function(req, res){
+app.get("/allData", function (req, res) {
 	console.log(req.url);
 	console.log(req.query);
 	var temp = req.query;
 	var data = temp.newData;
-	
-	urlNp = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+req.query.lat+","+req.query.lon+"&radius="+req.query.distance+"&type="+req.query.catu+"&keyword="+req.query.key+"&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
-	var urlNew=urlNp.replace(/ /g,'+');
+
+	urlNp = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + req.query.lat + "," + req.query.lon + "&radius=" + req.query.distance + "&type=" + req.query.catu + "&keyword=" + req.query.key + "&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
+	var urlNew = urlNp.replace(/ /g, '+');
 	console.log(urlNp);
-	var urlNew=encodeURI(urlNp);
+	var urlNew = encodeURI(urlNp);
 	request.get({
-	    url: urlNew,
-	    json: true,
-	    headers: {'User-Agent': 'request'}
-	  }, (err, response, data) => {
-	    if (err) 
-	    {
-	    	console.log('Error:', err);
-	    }
-	     else if (res.statusCode !== 200) {
-	    	console.log('Status:', response.statusCode);
-	    } else 
-	    {
-	      
-	      	 console.log(data);
-	      	res.setHeader('content-type', 'application/json');
+		url: urlNew,
+		json: true,
+		headers: { 'User-Agent': 'request' }
+	}, (err, response, data) => {
+		if (err) {
+			console.log('Error:', err);
+		}
+		else if (res.statusCode !== 200) {
+			console.log('Status:', response.statusCode);
+		} else {
+
+			console.log(data);
+			res.setHeader('content-type', 'application/json');
 			res.json(data);
-	    }
-	});    
+		}
+	});
 
 });
 
 
-app.get("/locWala", function(req, res){
+app.get("/locWala", function (req, res) {
 	//console.log(req.url);
 	//console.log(req.query);
 	var temp = req.query;
 	var data = temp.newLOC;
-	urlLoc = "https://maps.googleapis.com/maps/api/geocode/json?address="+req.query.location+"&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
+	urlLoc = "https://maps.googleapis.com/maps/api/geocode/json?address=" + req.query.location + "&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
 	console.log(urlLoc);
-		request.get(urlLoc, (error, response, body) => {
- 		 let json = JSON.parse(body);
-         console.log(json);
-         var urlNpLo=encodeURI(urlLoc);	 
-         console.log(urlNpLo);
-         var b= json.results[0].geometry.location.lat;
-         var c= json.results[0].geometry.location.lng;
-	urlNpLo = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+json.results[0].geometry.location.lat+","+json.results[0].geometry.location.lng+"&radius="+req.query.distance+"&type="+req.query.catu+"&keyword="+req.query.key+"&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
-	console.log(urlNpLo);
-	var urlNew2=encodeURI(urlNpLo);
-	//console.log(urlNew2);
-	request.get({
-	    url: urlNew2,
-	    json: true,
-	    headers: {'User-Agent': 'request'}
-	  }, (err, response, data) => {
-	    if (err) {
-	    	console.log('Error:', err);
-	    } 
-	    else if (res.statusCode !== 200) 
-	    {
-	    	console.log('Status:', response.statusCode);
-	    } else 
-	    {
-	   
-	      	 console.log(data);
-	      	res.setHeader('content-type', 'application/json');
-			res.json(data);
-	    }
-	});    
-})
+	request.get(urlLoc, (error, response, body) => {
+		let json = JSON.parse(body);
+		console.log(json);
+		var urlNpLo = encodeURI(urlLoc);
+		console.log(urlNpLo);
+		var b = json.results[0].geometry.location.lat;
+		var c = json.results[0].geometry.location.lng;
+		urlNpLo = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + json.results[0].geometry.location.lat + "," + json.results[0].geometry.location.lng + "&radius=" + req.query.distance + "&type=" + req.query.catu + "&keyword=" + req.query.key + "&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
+		console.log(urlNpLo);
+		var urlNew2 = encodeURI(urlNpLo);
+		//console.log(urlNew2);
+		request.get({
+			url: urlNew2,
+			json: true,
+			headers: { 'User-Agent': 'request' }
+		}, (err, response, data) => {
+			if (err) {
+				console.log('Error:', err);
+			}
+			else if (res.statusCode !== 200) {
+				console.log('Status:', response.statusCode);
+			} else {
+
+				console.log(data);
+				res.setHeader('content-type', 'application/json');
+				res.json(data);
+			}
+		});
+	})
 });
 
-app.get("/cadataAya", function(req, res){
+app.get("/cadataAya", function (req, res) {
 	//console.log(req.url);
 	//console.log(req.query);
 	//console.log(req.query.cadata);
@@ -98,59 +94,30 @@ app.get("/cadataAya", function(req, res){
 	var next = temp.cadata;
 	console.log(next);
 	console.log("agai value cadata kijjjjjjj");
-urlcadata = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken="+next+"&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
+	urlcadata = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" + next + "&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
 	//var urlcada=encodeURI(urlcadata);
-	console.log("urlcadata ki",urlcadata);
+	console.log("urlcadata ki", urlcadata);
 	request.get({
-	    url: urlcadata,
-	    json: true,
-	    headers: {'User-Agent': 'request'}
-	  }, (err, response, data) => {
-	    if (err) 
-	    {
-	    	console.log('Error:', err);
-	    } 
-	    else if (res.statusCode !== 200) 
-	    {
-	    	console.log('Status:', response.statusCode);
-	    } else {
-	      
-	      	 console.log(data);
-	      	res.setHeader('content-type', 'application/json');
+		url: urlcadata,
+		json: true,
+		headers: { 'User-Agent': 'request' }
+	}, (err, response, data) => {
+		if (err) {
+			console.log('Error:', err);
+		}
+		else if (res.statusCode !== 200) {
+			console.log('Status:', response.statusCode);
+		} else {
+
+			console.log(data);
+			res.setHeader('content-type', 'application/json');
 			res.json(data);
-	    }
-	});    
+		}
+	});
 
 });
-// app.get("/final", function(req, res){
-	
-// 	var placeIdi=req.query.placeIdio;
-// 	console.log(placeIdi);
-// 	console.log("placeididididi ki",placeIdi);
-// 	hw9Url="https://maps.googleapis.com/maps/api/place/details/json?placeid="+placeIdi+"&key=AIzaSyC00xSk8OLLyPTwJPMCTxjtOCOYrX-dW6A";
-// 	console.log("this is the placeidi wali url",hw9Url);
-// 	request.get({
-// 	    url: hw9Url,
-// 	    json: true,
-// 	    headers: {'User-Agent': 'request'}
-// 	  }, (err, response, data) => {
-// 	    if (err) 
-// 	    {
-// 	    	console.log('Error:', err);
-// 	    } 
-// 	    else if (res.statusCode !== 200) 
-// 	    {
-// 	    	console.log('Status:', response.statusCode);
-// 	    } else {
-	      
-// 	      	 console.log(data);
-// 	      	res.setHeader('content-type', 'application/json');
-// 			res.json(data);
-// 	    }
-// 	});    
 
-// });
-app.get("/yelpBanaing", function(req, res){
+app.get("/yelpBanaing", function (req, res) {
 	console.log(req.url);
 	console.log(req.query);
 	//console.log(req.query.cadata);
@@ -158,63 +125,58 @@ app.get("/yelpBanaing", function(req, res){
 	var next = temp.nameyelp;
 	console.log(next);
 	console.log("agai value cadata ki");
-yelpUrl = "https://api.yelp.com/v3/businesses/matches/best?name="+req.query.nameyelp+"&address1="+req.query.adddr+"&city="+req.query.cityzz+"&state="+req.query.statezz+"&country="+req.query.country;
-	var urlcanada=encodeURI(yelpUrl);
+	yelpUrl = "https://api.yelp.com/v3/businesses/matches/best?name=" + req.query.nameyelp + "&address1=" + req.query.adddr + "&city=" + req.query.cityzz + "&state=" + req.query.statezz + "&country=" + req.query.country;
+	var urlcanada = encodeURI(yelpUrl);
 	//console.log("urlcadata ki",urlcadata);
 	request.get({
-	    url: urlcanada,
-	    json: true,
-	    headers: {'User-Agent': 'request', 'Authorization': 'Bearer Njza3sFCRA9Ryj9ThYlmY38oXieiZrYlQvvTIe1DUnG89_VajllQVQntnGhSLpKHofkjdo7GYbDuQcyD0sgeaas3E5X6A31g6qMEzv1UmBg1p4EX_utsDkASwHPJWnYx'}
-	  }, (err, response, data) => {
-	    if (err)
-	     {
-	    	console.log('Error:', err);
-	    } 
-	    else if (res.statusCode !== 200) 
-	    {
-	    	console.log('Status:', response.statusCode);
-	    } else {
-	      // data already parsed
-	      	 console.log(data);
-	      	res.setHeader('content-type', 'application/json');
+		url: urlcanada,
+		json: true,
+		headers: { 'User-Agent': 'request', 'Authorization': 'Bearer Njza3sFCRA9Ryj9ThYlmY38oXieiZrYlQvvTIe1DUnG89_VajllQVQntnGhSLpKHofkjdo7GYbDuQcyD0sgeaas3E5X6A31g6qMEzv1UmBg1p4EX_utsDkASwHPJWnYx' }
+	}, (err, response, data) => {
+		if (err) {
+			console.log('Error:', err);
+		}
+		else if (res.statusCode !== 200) {
+			console.log('Status:', response.statusCode);
+		} else {
+			// data already parsed
+			console.log(data);
+			res.setHeader('content-type', 'application/json');
 			res.json(data);
-	    }
-	});    
+		}
+	});
 
 });
 
 
-app.get("/yelpKaTable", function(req, res){
+app.get("/yelpKaTable", function (req, res) {
 	console.log(req.url);
 	console.log(req.query);
 	//console.log(req.query.cadata);
 	var temp = req.query;
 	var next = temp.naviTaziId;
 	console.log(next);
-yelpFinalUrl = "https://api.yelp.com/v3/businesses/"+req.query.naviTaziId+"/reviews";
-	var urlcanadaAle=encodeURI(yelpFinalUrl);
+	yelpFinalUrl = "https://api.yelp.com/v3/businesses/" + req.query.naviTaziId + "/reviews";
+	var urlcanadaAle = encodeURI(yelpFinalUrl);
 	//console.log("urlcadata ki",urlcadata);
 	request.get({
-	    url: urlcanadaAle,
-	    json: true,
-	    headers: {'User-Agent': 'request', 'Authorization': 'Bearer Njza3sFCRA9Ryj9ThYlmY38oXieiZrYlQvvTIe1DUnG89_VajllQVQntnGhSLpKHofkjdo7GYbDuQcyD0sgeaas3E5X6A31g6qMEzv1UmBg1p4EX_utsDkASwHPJWnYx'}
-	  }, (err, response, data) => {
-	    if (err) 
-	    {
-	    	console.log('Error:', err);
-	    } else if (res.statusCode !== 200) 
-	    {
-	    	console.log('Status:', response.statusCode);
-	    } else 
-	    {
-	      	 console.log(data);
-	      	res.setHeader('content-type', 'application/json');
+		url: urlcanadaAle,
+		json: true,
+		headers: { 'User-Agent': 'request', 'Authorization': 'Bearer Njza3sFCRA9Ryj9ThYlmY38oXieiZrYlQvvTIe1DUnG89_VajllQVQntnGhSLpKHofkjdo7GYbDuQcyD0sgeaas3E5X6A31g6qMEzv1UmBg1p4EX_utsDkASwHPJWnYx' }
+	}, (err, response, data) => {
+		if (err) {
+			console.log('Error:', err);
+		} else if (res.statusCode !== 200) {
+			console.log('Status:', response.statusCode);
+		} else {
+			console.log(data);
+			res.setHeader('content-type', 'application/json');
 			res.json(data);
-	    }
-	});    
+		}
+	});
 
 });
 //start server
-app.listen(8081, function(){
-  console.log('Server started on port 8081..')
+app.listen(8081, function () {
+	console.log('Server started on port 8081..')
 });
